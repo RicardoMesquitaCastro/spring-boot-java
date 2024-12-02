3f5e5214635fa6dd4fa4c87a2d10725e90d6efa6
Ricardo Mesquita <ricardomesquita12@hotmail.com>
2024-12-02 16:46:53 -0300

    anatomia-de-classes

--- project-java/src/App.java
+++ project-java/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String primeiroNome = "Ricardo";
+        String segundoNome = "Mesquita";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
     }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }   
 }
